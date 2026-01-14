7c93b4eb8f80f59b342fe5cd67c71bedcec00007
Caelan ( <117565194+Caelan27@users.noreply.github.com>
2026-01-14 11:59:44 +0000

    Create homescreen and empty pages for sections

---
+++ main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from screens.game import GameScreen
+from screens.home import HomeScreen
+from screens.info import InfoScreen
+from screens.quiz import QuizScreen
+
+
+class HealthApp(App):
+    def build(self):
+        screen_manager = ScreenManager()
+
+        info_screen = InfoScreen(name="InfoScreen")
+        screen_manager.add_widget(info_screen)
+
+        home_screen = HomeScreen(name="HomeScreen")
+        screen_manager.add_widget(home_screen)
+
+        quiz_screen = QuizScreen(name="QuizScreen")
+        screen_manager.add_widget(quiz_screen)
+
+        game_screen = GameScreen(name="GameScreen")
+        screen_manager.add_widget(game_screen)
+
+        screen_manager.current = "HomeScreen"
+        return screen_manager
+
+
+if __name__ == "__main__":
+    HealthApp().run()
